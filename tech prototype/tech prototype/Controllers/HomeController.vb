59a564fc6e014b62fa4ea5a19fb3d13773de8b9c
Hristijan <hristijan2509@gmail.com>
2021-12-06 16:27:08 +0100

    Add tech prototype project

---
+++ tech prototype/tech prototype/Controllers/HomeController.vb
@@ -0,0 +1,29 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Message") = "Your application description page."
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "Your contact page."
+
+        Return View()
+    End Function
+    Function SavedATM() As ActionResult
+        ViewData("Message") = "Your Saved ATM."
+
+        Return View()
+    End Function
+    Function SearchATM() As ActionResult
+        ViewData("Message") = "Search ATM."
+
+        Return View()
+    End Function
+End Class
